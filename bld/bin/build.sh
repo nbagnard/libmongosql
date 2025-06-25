37ec06363b14664cf47186951f2cf5f152bdc615
Natacha Bagnard <91975317+nbagnard@users.noreply.github.com>
2025-06-25 09:41:26 -0700

    Add SSL_DIR for win64

--- bld/bin/build.sh
+++ bld/bin/build.sh
@@ -70,6 +70,7 @@ if [ "$PLATFORM" = macos ]; then
 elif [ "$PLATFORM" = win64 ]; then
     echo "SSL_DIR is \"$SSL_DIR\""
     SSL_SHORT_DIR="/usr/local/openssl"
+    mkdir -p $SSL_SHORT_DIR
     ln -s $SSL_DIR $SSL_SHORT_DIR
     CMAKE_ARGS="$CMAKE_ARGS -DWITH_SSL=\"$SSL_SHORT_DIR\" -DCMAKE_VERBOSE_MAKEFILE=ON"
 fi
