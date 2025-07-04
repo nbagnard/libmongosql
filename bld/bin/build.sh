c256d9b6ad12c5ba0df4da0416be1d3ba34af5ef
Natacha Bagnard <91975317+nbagnard@users.noreply.github.com>
2025-07-04 12:12:51 -0700

    Use OpenSSL system

--- bld/bin/build.sh
+++ bld/bin/build.sh
@@ -67,15 +67,15 @@ if [ "$PLATFORM" = macos ]; then
     unzip openssl-1.0.2n.zip
     SSL_DIR="$BUILD_DIR/1.0.2n"
     CMAKE_ARGS="$CMAKE_ARGS -DWITH_SSL=$SSL_DIR -DCMAKE_VERBOSE_MAKEFILE=ON"
-elif [ "$PLATFORM" = win64 ]; then
-    ls -lrt "$SSL_DIR"
-    echo "SSL_DIR is \"$SSL_DIR\""
-    SSL_SHORT_DIR="$ARTIFACTS_DIR/openssl_symlink"
-    ln -s "$SSL_DIR" $SSL_SHORT_DIR
-    ls -lrt $SSL_SHORT_DIR
-    ls -lrt "$SSL_SHORT_DIR/include"
-    ls -lrt "$SSL_SHORT_DIR/include/openssl"
-    CMAKE_ARGS="$CMAKE_ARGS -DWITH_SSL=\"$SSL_SHORT_DIR\" -DCMAKE_VERBOSE_MAKEFILE=ON"
+#elif [ "$PLATFORM" = win64 ]; then
+#    ls -lrt "$SSL_DIR"
+#    echo "SSL_DIR is \"$SSL_DIR\""
+#    SSL_SHORT_DIR="$ARTIFACTS_DIR/openssl_symlink"
+#    ln -s "$SSL_DIR" $SSL_SHORT_DIR
+#    ls -lrt $SSL_SHORT_DIR
+#    ls -lrt "$SSL_SHORT_DIR/include"
+#    ls -lrt "$SSL_SHORT_DIR/include/openssl"
+#    CMAKE_ARGS="$CMAKE_ARGS -DWITH_SSL=\"$SSL_SHORT_DIR\" -DCMAKE_VERBOSE_MAKEFILE=ON"
 fi
 
 $PROJECT_ROOT/bld/bin/build-icu.sh
--- bld/bin/platforms.sh
+++ bld/bin/platforms.sh
@@ -74,7 +74,7 @@ win64)
     PLATFORM_ARCH='64'
     PLATFORM_NAME='windows'
 	# Use OpenSSL on Windows.
-	  SSL_DIR='/cygdrive/c/Program Files/OpenSSL-Win64'
+	  #SSL_DIR='/cygdrive/c/Program Files/OpenSSL-Win64'
     #CMAKE_ARGS="$CMAKE_ARGS -DWITH_SSL=\"C:/Program\ Files/OpenSSL-Win64\""
     #CMAKE_GENERATOR='Visual Studio 14 2015 Win64'
     CMAKE_GENERATOR='Visual Studio 16 2019'
